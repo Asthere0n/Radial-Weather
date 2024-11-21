405f7c9e442e36d3528b6ddfd1f71f2e12549ddc
Asthere0n <cristiansanchezsonido@gmail.com>
2024-11-21 03:04:56 +0000

    Added all the IDs needed to the HTML and compiled them into HTML address.JS

---
+++ src/components/HTML address.js
@@ -0,0 +1,38 @@
+//HTML of Location 0
+const location0City = document.getElementById("location0City").innerHTML
+const location0Country = document.getElementById("location0Country").innerHTML
+const location0CountryFlag = document.getElementById("location0CountryFlag")
+const location0WeatherIcon = document.getElementById("location0WeatherIcon")
+const location0Temperature = document.getElementById("location0Temperature").innerHTML
+const location0LocalTime = document.getElementById("location0LocalTime").innerHTML
+const imgSkyWeather = document.getElementById("imgSkyWeather")
+
+//HTML of Location 90
+const location90City = document.getElementById("location90City").innerHTML
+const location90Country = document.getElementById("location90Country").innerHTML
+const location90CountryFlag = document.getElementById("location90CountryFlag")
+const location90WeatherIcon = document.getElementById("location90WeatherIcon")
+const location90Temperature = document.getElementById("location90Temperature").innerHTML
+const location90LocalTime = document.getElementById("location90LocalTime").innerHTML
+
+//HTML of Location 180
+const location180City = document.getElementById("location180City").innerHTML
+const location180Country = document.getElementById("location180Country").innerHTML
+const location180CountryFlag = document.getElementById("location180CountryFlag")
+const location180WeatherIcon = document.getElementById("location180WeatherIcon")
+const location180Temperature = document.getElementById("location180Temperature").innerHTML
+const location180LocalTime = document.getElementById("location180LocalTime").innerHTML
+
+//HTML of Location 270
+const location270City = document.getElementById("location270City").innerHTML
+const location270Country = document.getElementById("location270Country").innerHTML
+const location270CountryFlag = document.getElementById("location270CountryFlag")
+const location270WeatherIcon = document.getElementById("location270WeatherIcon")
+const location270Temperature = document.getElementById("location270Temperature").innerHTML
+const location270LocalTime = document.getElementById("location270LocalTime").innerHTML
+
+export {location0City, location0Country, location0CountryFlag, location0WeatherIcon, location0LocalTime, location0Temperature, imgSkyWeather,
+    location90City, location90Country, location90CountryFlag, location90WeatherIcon, location90LocalTime, location90Temperature,
+    location180City, location180Country, location180CountryFlag, location180WeatherIcon, location180LocalTime, location180Temperature,
+    location270City, location270Country, location270CountryFlag, location270WeatherIcon, location270LocalTime, location270Temperature
+}
