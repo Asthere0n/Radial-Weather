e28c7dd885e23655ef2f2e00fbfd970746413c2b
Asthere0n <cristiansanchezsonido@gmail.com>
2024-11-25 00:00:32 +0000

    introduced the API Key.

--- src/api/fetch_Weather.js
+++ src/api/fetch_Weather.js
@@ -1,3 +1,17 @@
-function fetchWeatherData(location){
-    
-}
+import {weatherAPI_Key} from "./API_Key.js"
+
+export async function getWeather(data){
+    const weatherAPI_URL = `https://api.openweathermap.org/data/3.0/onecall?lat={${data.latitude}}&lon={${data.longitude}}&exclude={hourly,daily,minutely}&appid={${weatherAPI_Key}}`
+    console.log(weatherAPI_URL)
+    fetch(weatherAPI_URL)
+    .then((Response, error)=>{
+        console.log(Response)
+        if (!Response.ok){
+            throw new Error (error)
+        } else {
+            console.log(Response)
+            return Response
+        }
+    })
+    .catch(error => console.error("Error fetching the weather data: ",error))
+    }
--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
 import { getLocation } from "./api/fetch_Geolocation.js";
 import { location0LocalTime } from "./components/HTML_address.js";
+import { getWeather } from "./api/fetch_Weather.js";
 
 let currentLocation = {};
+let currentTime
+let currentWeather;
 
-getLocation()
-  .then((data) => {
-    currentLocation = data.position.coords;
-    const currentTime = new Date(data.position.timestamp);
-    location0LocalTime.innerHTML = `${currentTime.getHours()}:${currentTime.getMinutes()}`;
-  })
-  .then(() => {
-    console.log("completed");
-    console.log(currentLocation);
-  })
-  .catch((error) => console.error(error));
+async function fetchingData() {
+  await getLocation()
+    .then((data) => {
+      currentLocation.latitude = data.position.coords.latitude;
+      currentLocation.longitude = data.position.coords.longitude;
+
+      currentTime = new Date(data.position.timestamp);
+      location0LocalTime.innerHTML = `${currentTime.getHours()}:${currentTime.getMinutes()}`;
+    })
+    .then(() => {
+      currentWeather = getWeather(currentLocation);
+    })
+    .then(() => {
+      console.log("completed");
+      console.log(currentLocation);
+      console.log(currentWeather);
+    });
+  //.catch((error) => console.error(error));
+}
+
+fetchingData();
